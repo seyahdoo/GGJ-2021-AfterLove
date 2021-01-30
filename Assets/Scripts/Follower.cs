8132a6d3bea15afff13d26329165a8cd715542a5
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-01-30 21:04:21 +0300

    follow

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+    public Transform followee;
+    private void Update() {
+        transform.position = Vector3.Lerp(transform.position, followee.position, .1f);
+    }
+}
