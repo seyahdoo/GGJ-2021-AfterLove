bdab97d08164a42e19b276ed7255fe3b5d35eb51
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-01-31 20:57:44 +0300

    keyboard input

--- Assets/Game/Joypad/Joypad.cs
+++ Assets/Game/Joypad/Joypad.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Joypad : MonoBehaviour {
@@ -8,30 +9,37 @@ public class Joypad : MonoBehaviour {
     [SerializeField] private RectTransform joyPad;
     [SerializeField] private float touchHorizontalDeadzonePercentage = 0f;
     [SerializeField] private float touchVerticalDeadzonePercentage = 0f;
-    [SerializeField] private float joypadOverallDeadzone = .1f;
+    [SerializeField] public float joypadOverallDeadzone = .1f;
     [SerializeField] private float sensitivity = .05f;
     [SerializeField] private bool normalizeInput = true;
     [SerializeField] private Animator joyPadAnimator;
     [SerializeField] private string animatorHorizontalParamName = "blendX";
     [SerializeField] private string animatorVerticalParamName = "blendY";
+    [SerializeField] public string horizontal;
+    [SerializeField] public string vertical;
+    [SerializeField] public KeyCode[] keys;
+    
     private const int NON_POINTER = -2973642;
     private int _startPointerId;
     private Vector2 _startPoint;
+    public bool _unityInputEnabled;
+    public int _pressedKeys;
     public void Init() {
         _startPointerId = NON_POINTER;
         joyPad.gameObject.SetActive(false);
         tapped = false;
         tapEnabled = true;
+        _unityInputEnabled = true;
         input = Vector2.zero;
     }
     public void OnPointerDown(PointerEventData eventData) {
         if (_startPointerId == NON_POINTER) {
+            _unityInputEnabled = false;
             joyPad.gameObject.SetActive(true);
             _startPointerId = eventData.pointerId;
             _startPoint = eventData.position;
             joyPad.position = _startPoint;
             if (tapEnabled) tapped = true;
-            UpdateJoypadGraphic(Vector2.zero);
         }
     }
     public void OnDrag(PointerEventData eventData) {
@@ -48,6 +56,7 @@ public class Joypad : MonoBehaviour {
     }
     public void OnPointerUp(PointerEventData eventData) {
         if (eventData.pointerId == _startPointerId) {
+            _unityInputEnabled = true;
             joyPad.gameObject.SetActive(false);
             _startPointerId = NON_POINTER;
             input = Vector2.zero;
---
+++ Assets/KeyboardInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour {
+    public Joypad[] joypads;
+    private void Update() {
+        foreach (var joypad in joypads) {
+            if (joypad._unityInputEnabled) {
+                var horizontalInput = Input.GetAxis(joypad.horizontal);
+                var verticalInput = Input.GetAxis(joypad.vertical);
+                var unityInput = new Vector2(horizontalInput, verticalInput);
+                joypad.input = unityInput.magnitude > joypad.joypadOverallDeadzone ? unityInput : Vector2.zero;
+                foreach (var keyCode in joypad.keys) {
+                    if (Input.GetKeyDown(keyCode)) {
+                        if (joypad._pressedKeys == 0) {
+                            joypad._pressedKeys++;
+                            if (joypad.tapEnabled) {
+                                joypad.tapped = true;
+                            }
+                        }
+                    }
+                    if (Input.GetKeyUp(keyCode)) joypad._pressedKeys--;
+                    joypad._pressedKeys = Mathf.Clamp(joypad._pressedKeys, 0, 4);
+                }
+            }
+        }
+    }
+}
