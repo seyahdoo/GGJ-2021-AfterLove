858f40aed7bad4e21146bfdfef22750a6f2d70f7
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-01-31 18:03:06 +0300

    bed anim fix

--- Assets/JumpPad.cs
+++ Assets/JumpPad.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 public class JumpPad : MonoBehaviour {
     public Vector3 directionForce = Vector3.up * 10f;
+    public Animator animator;
+    private static readonly int Activate = Animator.StringToHash("activate");
     private void OnCollisionEnter(Collision other) {
         var worldDirection = transform.TransformDirection(directionForce);
-        other.rigidbody.velocity = worldDirection;
+        if (other.rigidbody != null) {
+            other.rigidbody.velocity = worldDirection;
+            animator.SetTrigger(Activate);
+        }
     }
 }
