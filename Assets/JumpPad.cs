fb05ae0c8fddaf186148e5f945c5aa404b4f7113
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-01-31 17:37:01 +0300

    bed jump pad

---
+++ Assets/JumpPad.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+public class JumpPad : MonoBehaviour {
+    public Vector3 directionForce = Vector3.up * 10f;
+    private void OnCollisionEnter(Collision other) {
+        var worldDirection = transform.TransformDirection(directionForce);
+        other.rigidbody.velocity = worldDirection;
+    }
+}
